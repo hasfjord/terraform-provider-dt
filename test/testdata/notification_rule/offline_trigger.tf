7454ce93294ef6f058b838257efccd49d3db58ed
thomahas <thomas.hasfjord@disruptive-technologies.com>
2025-04-09 12:05:30 +0200

    fix: corrected panic when using alternative triggers
    Added testdata folders for notification rules.

---
+++ test/testdata/notification_rule/offline_trigger.tf
@@ -0,0 +1,28 @@
+# Copyright (c) HashiCorp, Inc.
+
+resource "dt_notification_rule" "test" {
+  display_name = "Cloud connector offline"
+  project_id   = dt_project.test.id
+
+  trigger = {
+    field      = "connectionStatus"
+    connection = "CLOUD_CONNECTOR_OFFLINE"
+  }
+  escalation_levels = [
+    {
+      display_name = "Escalation Level 1"
+      actions = [
+        {
+          type = "EMAIL"
+          email_config = {
+            body = "Cloud connector is offline"
+            recipients = [
+              "this.guy@example.com"
+            ]
+            subject = "Cloud connector offline alert"
+          }
+        }
+      ]
+    }
+  ]
+}
